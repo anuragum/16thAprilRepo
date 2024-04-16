b9da4efa0abee69bf1fe0bc42e04215213c2360d
anuragumarkhede <anuragumarkhede1995@gmail.com>
2024-04-16 11:28:32 +0530

    soem files added

---
+++ mapFunctionDemo2.js
@@ -0,0 +1,12 @@
+// Create an empty Map and use Map.set() to add key-value pairs
+const setMap = new Map();
+
+// Adding key-value pairs using Map.set()
+setMap.set('name', 'John');
+setMap.set('age', 25);
+setMap.set('city', 'New York');
+
+// Accessing values in the Map
+console.log(setMap.get('name')); // Output: "John"
+console.log(setMap.get('age')); // Output: 25
+console.log(setMap.get('city')); // Output: "New York"
